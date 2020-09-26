3a406e9e329407a29f4e512118e64d549815bd4d
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2020-09-26 18:32:42 +0530

    20_SEP_2020

--- 3RI_PROJECTS/myHospital-master/loginmodule/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from loginmodule.views import auth_view
-from django.contrib.auth import views as auth_views
-from django.conf.urls import url
-
-urlpatterns = [
-	path('auth', auth_view),
-]
