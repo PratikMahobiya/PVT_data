69059a0db166dbf8895a9d58616e61f8506fcd3b
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2020-09-28 12:13:38 +0530

    Update make_call.py

--- make_call.py
+++ make_call.py
@@ -4,13 +4,13 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACfa44115d0ff5f4602437ca7a4130b110'
-auth_token = 'e054d9870d54ba655fa932e3ffabb83d'
+account_sid = 'YOUR SID'
+auth_token = 'YOUR TOKEN'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to='+919893578798',
+                        to='+91xxxxxxxxxx',
                         from_='+19046377572'
                     )
 
