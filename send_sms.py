bda35ab39db136c0a12c82f5dec3f762715e3d25
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2020-09-28 12:14:32 +0530

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACfa44115d0ff5f4602437ca7a4130b110'
-auth_token = 'e054d9870d54ba655fa932e3ffabb83d'
+account_sid = 'YOUR SID'
+auth_token = 'YOUR TOKEN'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
@@ -13,7 +13,7 @@ message = client.messages \
                      body="Hello,\n This is Trail sms from Pratik Mahobiya\n to check the working of Program.\n\n Thank You",
                      from_='+19046377572',
                      #media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'],
-                     to='+917000681073'
+                     to='+91xxxxxxxxxx'
                  )
 
 print(message.sid)
